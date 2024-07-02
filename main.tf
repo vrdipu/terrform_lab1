a439ac379612480cc0e2d6a425736716df0e653f
vrdipu <vrdipu@gmail.com>
2024-07-02 15:44:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,10 +25,17 @@ resource "aws_instance" "k8s_node" {
               sudo apt-get install -y docker.io
               sudo systemctl start docker
               sudo systemctl enable docker
-              curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+              [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+              [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-arm64
               chmod +x ./kind
               sudo mv ./kind /usr/local/bin/kind
-              kind create cluster --name test
+              kind create cluster --name devops2023
+              sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+              curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+              echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+              sudo apt-get update
+              sudo apt-get install -y  kubectl
+              kubectl cluster-info --context kind-test
               EOF
 
   tags = {
